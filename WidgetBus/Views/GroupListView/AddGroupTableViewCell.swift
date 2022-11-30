681707dc2bc90526b5cb85d53b22424a205ecae3
Shin-jun <inside1451@naver.com>
2022-11-30 10:54:24 +0900

    [fix] Change AddGroupTableViewCell
    라벨변경

--- WidgetBus/Views/GroupListView/AddGroupTableViewCell.swift
+++ WidgetBus/Views/GroupListView/AddGroupTableViewCell.swift
@@ -10,19 +10,12 @@ import UIKit
 class AddGroupTableViewCell: UITableViewCell {
 
     private lazy var groupListAddLabel: UILabel = {
-          let label = UILabel()
-        let attachment = NSTextAttachment()
-        attachment.image = UIImage(systemName: "plus")
-        attachment.bounds = CGRect(x: 0, y: 0, width: 40, height: 38)
-        let attachmentString = NSAttributedString(attachment: attachment)
-        let contentString = NSMutableAttributedString(string: "")
-        contentString.append(attachmentString)
-        label.attributedText = contentString
-        label.sizeToFit()
-//        label.text = "+"
-//        label.font = .systemFont(ofSize: 70, weight: .bold)
+        let label = UILabel()
+        label.text = "+"
+        label.font = .systemFont(ofSize: 70, weight: .bold)
         label.textAlignment = .center
         label.textColor = .white
+
         label.backgroundColor = .duduDeepBlue
         label.layer.masksToBounds = true
         label.layer.opacity = 0.3
@@ -37,18 +30,11 @@ class AddGroupTableViewCell: UITableViewCell {
         return label
     }()
 
-    private lazy var addImageView: UIImageView = {
-        let image = UIImageView()
-        image.image = UIImage(named: "duduPlus")
-        image.translatesAutoresizingMaskIntoConstraints = false
-
-        return image
-    }()
-
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         setupLayout()
     }
+
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
